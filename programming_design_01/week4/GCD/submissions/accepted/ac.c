67007e7027567aa0051b2a1816c2f5949941447e
as6325400 <as6325400@gmail.com>
2024-03-10 12:03:44 +0800

    re testcase

--- programming_design_01/week4/GCD/submissions/accepted/ac.c
+++ programming_design_01/week4/GCD/submissions/accepted/ac.c
@@ -2,11 +2,35 @@
 #include<stdlib.h>
 #define int long long
 
-int arr[200005]
+int arr[200005];
 
 signed main(){
   int n;
   scanf("%lld", &n);
-
+  int min = 1e9;
+  for(int i = 0; i < n; i++){
+    scanf("%lld", &arr[i]);
+    if(arr[i] < min) min = arr[i];
+  }
+  int start = min;
+  int ans = 1;
+  while (start >= 0)
+  { 
+    int flag = 0;
+    for(int i = 0; i < n; i++){
+      if(arr[i] % start != 0){
+        flag = 1;
+        break;
+      }
+    }
+    if(flag == 0){
+      ans = start;
+      break;
+    }
+    start--;
+  }
+  printf("%lld\n", ans);
+  
+  
   return 0;
 }
