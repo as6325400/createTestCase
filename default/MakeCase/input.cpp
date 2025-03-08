41ff31703d38b6adb8cd77689ac4deee8ad64a59
as6325400 <as6325400@gmail.com>
2025-03-08 16:04:49 +0800

    update default input

--- default/MakeCase/input.cpp
+++ default/MakeCase/input.cpp
@@ -1,14 +1,14 @@
 #include"testlib.h"
 #include<bits/stdc++.h>
-#define int long long
 
 using namespace std;
 
-signed main(int argc, char* argv[]){
+int main(int argc, char* argv[]){
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   int teseCaseNum = 10;
   registerGen(argc, argv, 1);
+
   for(int i = 2; i <= teseCaseNum; i++){
     string path = "../data/secret/";
     string id;
