04bbfa445794125fa37169c562f1a9532bc9d47a
as6325400 <as6325400@gmail.com>
2025-03-08 16:00:25 +0800

    default input

--- default/MakeCase/input.cpp
+++ default/MakeCase/input.cpp
@@ -1,16 +1,13 @@
+#include"testlib.h"
 #include<bits/stdc++.h>
 #define int long long
 
 using namespace std;
 
-signed main(){
+signed main(int argc, char* argv[]){
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   int teseCaseNum = 10;
-  srand(time(nullptr));
-  std::random_device rd;  
-  std::mt19937 gen(rd()); 
-  std::uniform_int_distribution<int> dis(1, 1000000000);
 
   for(int i = 2; i <= teseCaseNum; i++){
     string path = "../data/secret/";
